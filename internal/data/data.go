5a26acd704a7872e7966b4af8a5da6bc9aecad6a
joe <joewttx@163.com>
2023-03-13 22:57:02 +0800

    feat: add tm-db

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,10 +1,10 @@
 package data
 
 import (
-	"kvdbproxy/internal/conf"
-
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	dbm "github.com/tendermint/tm-db"
+	"kvdbproxy/internal/conf"
 )
 
 // ProviderSet is data providers.
@@ -12,7 +12,7 @@ var ProviderSet = wire.NewSet(NewData, NewKvdbRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	db dbm.DB
 }
 
 // NewData .
@@ -20,5 +20,13 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	// TODO: 这里默认使用GoLevelDB调试，后续根据请求header进行动态调用
+	db, err := dbm.NewDB(c.Database.Name, dbm.GoLevelDBBackend, c.Database.Dir)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &Data{
+		db: db,
+	}, cleanup, nil
 }
